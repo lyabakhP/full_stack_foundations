4198f0744bb6f7468b5d41bf23746a2c2e6fdc17
anazema <lyabakhp@gmail.com>
2018-02-03 19:34:33 +0200

    created mapper

--- sqlalchemy_test/database_setup.py
+++ sqlalchemy_test/database_setup.py
@@ -11,8 +11,16 @@ Base.metadata.create_all(engine)# add tables to db
 
 class Restaurant(Base):
     __tablename__='restaurant'
+    name = Column(String(80),nullable=False)
+    id = Column(Integer, primary_key=True)
 
 class MenuItem(object):
     __tablename__='menu_item'
-
+    name = Column(String(80),nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
         
